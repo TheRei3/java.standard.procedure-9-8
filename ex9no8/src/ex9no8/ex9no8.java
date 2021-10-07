fe24b7a09f4c63cbebec3a87cc83a32a6c29a597
admin <admin@host.docker.internal>
2021-10-07 09:13:20 +0900

    ex9no8

---
+++ ex9no8/src/ex9no8/ex9no8.java
@@ -0,0 +1,15 @@
+package ex9no8;
+
+import java.util.Arrays;
+
+public class ex9no8 {  // 458p
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4, 5};
+		int[] arrClone = arr.clone();
+		arrClone[0] = 6;
+		
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arrClone));
+	}
+
+}
